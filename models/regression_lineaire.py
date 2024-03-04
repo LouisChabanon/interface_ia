ac90ede9d895d72f3e55e9f491c6684b4ae251ee
Lordva <louis.chabanon@gmail.com>
2024-03-04 13:03:16 +0100

    ajout choix parametres

--- models/regression_lineaire.py
+++ models/regression_lineaire.py
@@ -1,4 +1,4 @@
-from models.utils import Model, ModelType
+from models.utils import Model, ModelType, separate_data
 import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -11,15 +11,25 @@ class RegressionLineaire(Model):
         self.param = None
         self.name = "Regression linéaire"
 
-    def run(self, data: dict[pd.DataFrame], param: dict):
+    def run(self, data: pd.DataFrame, param: list):
         model = LinearRegression()
-        training_data = data["training_data"]
-        predict_data = data["predict_data"]
-        x_index, y_index = param["x_index"], param["y_index"]
-        X, Y = training_data[[x_index]], training_data[[y_index]]
+        separeted_data = separate_data(data, param[2])
+        training_data = separeted_data["training_data"]
+        predict_data = separeted_data["predict_data"]
+        x_index, y_index = param[0], param[1]
+        X, Y = training_data[x_index], training_data[y_index]
         reg = model.fit(X, Y)
 
-        return reg.predict(predict_data[["Heure"]])
+        return reg.predict(predict_data[x_index])
+
+    def display_parameters(self):
+        st.write("Colonnes à utiliser pour la régression linéaire")
+        x_index = st.text_input("Nom de la colonne x", "Heure")
+        y_index = st.text_input("Nom de la colonne y", "Valeur")
+        st.write("Part de l'échantillon pour l'entrainement")
+        ratio = st.slider("Ratio", 0.1, 1.0, 0.8)
+
+        return [x_index, y_index, ratio]
 
     def display_results(self):
         st.write(f"Résultat du modèle {self.name} avec paramètre {self.param}")
